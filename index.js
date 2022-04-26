51f64645883fb35f71b35909cf72111ba2ec4c5e
alvino.barboza <alvinobarboza@gmail.com>
2022-04-26 16:39:45 -0300

    Validation functions implementation

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
+const { reportAllcustomers, reportVodWatched, reportVodPackage } = require("./util/dataModel"); //testing only
 const { 
     getAllCustomers, 
     getVodsPackages, 
     getWatchedVods,
 } = require("./util/reports");
+const { customersPackagesValidation, groupVodsByWatchedAmount, vodsPackageValidation } = require("./util/validation");
 
-/*Promise.all(
-    [
-        getAllCustomers(),
-        getVodsPackages(),
-        getWatchedVods()
+// Promise.all(
+//     [
+//         getAllCustomers(),
+//         getVodsPackages(),
+//         getWatchedVods()
+//     ]
+// ).then( data => {
+//     const allCustomers = data[0].response.rows;
+//     const vodsPackages = data[1].response.rows;
+//     const vodsWatched = data[2].response.rows;
+//     const customersValidation = customersPackagesValidation(allCustomers);
+//     const vodsValidation = customersPackagesValidation(vodsWatched);
+
+//     const table = [
+//         {
+//             studios:vodsValidation[0].length,
+//             nK:vodsValidation[1].length,
+//             rest:vodsValidation[2].length
+//         },
+//         {
+//             studios:customersValidation[0].length,
+//             nK:customersValidation[1].length,
+//             rest:customersValidation[2].length
+//         },
+//     ]
+//     console.table(table);
+// });
+
+const main = () => {
+
+    vodsPackageValidation(reportVodWatched, reportVodPackage);
+    const customersValidation = customersPackagesValidation(reportAllcustomers);
+    const vodsValidation = customersPackagesValidation(reportVodWatched);
+    const table = [
+        {
+            studios:vodsValidation[0].length,
+            nK:vodsValidation[1].length,
+            rest:vodsValidation[2].length
+        },
+        {
+            studios:customersValidation[0].length,
+            nK:customersValidation[1].length,
+            rest:customersValidation[2].length
+        },
     ]
-).then( data => {
-    console.table(data[0].response.rows)
-})*/
+    console.table(table);
+    console.log('Studios');
+    console.table(groupVodsByWatchedAmount(vodsValidation[0]));
+    console.log('Nacionais/kids');
+    console.table(groupVodsByWatchedAmount(vodsValidation[1]));
+    console.log('Resto');
+    console.table(groupVodsByWatchedAmount(vodsValidation[2]));
+}
 
-
-getAllCustomers().then(data=>{
-    const studios = data.response.rows.filter(data =>  data.products.toLowerCase().includes('studio'));
-    const nacionaisKids = data.response.rows.filter(data => (data.products.toLowerCase().includes('svod') && !(data.products.toLowerCase().includes('studio'))));    
-    const rest = data.response.rows.filter(data => !(data.products.toLowerCase().includes('svod')));
-    console.table([data.response.rows.length,studios.length, nacionaisKids.length, rest.length]);
-});
-
+main();
---
+++ util/validation.js
@@ -0,0 +1,61 @@
+const vodsPackageValidation = (vodsWatched, vodsPackages) => {
+    vodsWatched.forEach((v, i) => {
+        let found = false;
+        vodsPackages.forEach(vp=>{
+            if(v.vodsid === vp.vodsid){
+                vodsWatched[i].vodpackage = vp.package
+                found = true;
+            }
+        });
+        if(!found){
+            vodsWatched[i].vodpackage = '';
+        }
+    })
+}
+
+const customersPackagesValidation = (unvalidated) => {
+    const studios = unvalidated.filter(data =>  data.package.toLowerCase().includes('studio'));
+    const nacionaisKids = unvalidated.filter(data => (data.package.toLowerCase().includes('svod') && !(data.package.toLowerCase().includes('studio'))));    
+    const rest = unvalidated.filter(data => !(data.package.toLowerCase().includes('svod')));
+    return [studios,nacionaisKids,rest];
+}
+
+const groupVodsByWatchedAmount = (vods) => {
+    const vodsGroupSet = new Set();
+    vods.forEach(vod => {
+        if(vod.vodpackage === 'SVOD Kids' || vod.vodpackage === 'SVOD Nacional' || vod.vodpackage === 'SVOD Studio'){
+            vodsGroupSet.add(vod.vodsid);
+        }
+    });
+
+    const vodsGrooupArray = [];
+
+    vodsGroupSet.forEach(vod => {
+        vodsGrooupArray.push({
+            vodsid: vod,
+        })
+    });
+
+    vodsGrooupArray.forEach((vodGrouped, index) => {
+        let counter = 0;
+        let vodName = '';
+        let vodPackage = '';
+        vods.forEach(v => {
+            if(v.vodsid === vodGrouped.vodsid){
+                counter++;
+                vodName = v.vod;
+                vodPackage = v.vodpackage;
+            }
+        })
+        vodsGrooupArray[index].vodname = vodName;
+        vodsGrooupArray[index].package = vodPackage;
+        vodsGrooupArray[index].amountwatched = counter;
+    });
+    return vodsGrooupArray;
+}
+
+module.exports = {
+    customersPackagesValidation,
+    groupVodsByWatchedAmount,
+    vodsPackageValidation
+};
