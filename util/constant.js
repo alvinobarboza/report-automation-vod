15ff6e0531c18c877d61122c1b6eb56fb631070e
Alvino Barboza <alvinobarboza@gmail.com>
2024-10-28 17:14:22 -0300

    New report constants

--- util/constant.js
+++ util/constant.js
@@ -1,3 +1,5 @@
+const { getCurrentDateForScheduleHistory } = require('./date');
+
 const SMSURLYPLAY = 'https://sms.yplay.com.br';
 const MWURLYPLAY = 'https://mw.yplay.com.br';
 const SMSURLTIP = 'https://sms.tvnsul.com.br';
@@ -5,23 +7,74 @@ const MWURLTIP = 'https://mw.tvnsul.com.br';
 const SMSURLSUMICITY = 'https://sms.sumicity.net.br';
 const MWURLSUMICITY = 'https://mw.sumicity.net.br';
 const REPORT = '/api/report/reportSelection';
+const REPORT_SCHEDULE = '/api/reportSchedule/downloadReport';
+const REPORT_SCHEDULE_HISTORY = '/api/reportSchedule/historySelection';
 
+/**
+ * @param {number} id
+ * @returns {string}
+ */
 const smsBody = (id) => `{
     "data":{
         "reports_id": ${id}
     }
 }`;
 
+/**
+ * @param {number} id
+ * @returns {string}
+ */
 const mwBody = (id) => `{
     "data":{
         "reportsId": ${id}
     }
 }`;
 
+/**
+ * @param {number} id
+ * @returns {string}
+ */
+const lastestEntryFromScheduleBody = (id) => {
+    return `{
+        "data": {
+            "where": [
+                {
+                    "column": "report_schedules_attachements_report_schedules_id",
+                    "type": "=",
+                    "valueType": "%i",
+                    "value": ${id}
+                },
+                {
+                    "column": "report_schedules_attachements_generated",
+                    "type": ">=",
+                    "valueType": "%t",
+                    "value": "${getCurrentDateForScheduleHistory()}"
+                }
+            ]
+        }
+    }
+    `;
+};
+
+/**
+ * @param {number} id
+ * @returns {string}
+ */
+const downloadReportBody = (id) =>
+    `{ "data": { "reportSchedulesAttachementsId": ${id} } }`;
+
+/**
+ * @param {string} token
+ * @returns {{Authorization:string}}
+ */
 const smsHeader = (token) => {
     return { Authorization: token };
 };
 
+/**
+ * @param {string} token
+ * @returns {{'Authorization-user':string}}
+ */
 const mwHeader = (token) => {
     return { 'Authorization-user': token };
 };
@@ -34,8 +87,12 @@ module.exports = {
     SMSURLSUMICITY,
     MWURLSUMICITY,
     REPORT,
+    REPORT_SCHEDULE,
+    REPORT_SCHEDULE_HISTORY,
     smsBody,
     mwBody,
+    downloadReportBody,
+    lastestEntryFromScheduleBody,
     mwHeader,
     smsHeader,
 };
