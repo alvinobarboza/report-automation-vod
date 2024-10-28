8f87a120f816ed018911b37d8e52c237cebccfb4
Alvino Barboza <alvinobarboza@gmail.com>
2024-10-28 17:15:38 -0300

    New date function

--- util/date.js
+++ util/date.js
@@ -1,55 +1,68 @@
 const getDate = () => {
     const tempDate = new Date();
-    return (new Date(tempDate.getFullYear(), tempDate.getMonth() - 1)).toLocaleString('pt-br');
-}
+    return new Date(
+        tempDate.getFullYear(),
+        tempDate.getMonth() - 1
+    ).toLocaleString('pt-br');
+};
 
 const getLastMonthYearNumeric = () => {
     const tempDate = new Date();
-    return (new Date(tempDate.getFullYear(), tempDate.getMonth() - 1))
+    return new Date(tempDate.getFullYear(), tempDate.getMonth() - 1)
         .toLocaleString('pt-BR', { month: 'numeric', year: 'numeric' })
         .split('/')
         .join('.');
-}
+};
 
 const getCurrentMonth = () => {
     const tempDate = new Date();
-    return (new Date(tempDate.getFullYear(), tempDate.getMonth() - 1))
+    return new Date(tempDate.getFullYear(), tempDate.getMonth() - 1)
         .toLocaleString('pt-BR', { month: 'short' })
         .toLocaleUpperCase()
         .split('.')[0];
-}
+};
 
 const getCurrentMonthLong = () => {
     const tempDate = new Date();
-    return (new Date(tempDate.getFullYear(), tempDate.getMonth() - 1))
+    return new Date(tempDate.getFullYear(), tempDate.getMonth() - 1)
         .toLocaleString('pt-BR', { month: 'long' })
         .toLocaleUpperCase()
         .split('.')[0];
-}
+};
 
 const getCurrentYear = () => {
-    return (new Date).getFullYear();
-}
+    return new Date().getFullYear();
+};
 
 const getLastMonthYearShort = () => {
     const tempDate = new Date();
-    const date = (new Date(tempDate.getFullYear(), tempDate.getMonth() - 1))
+    const date = new Date(tempDate.getFullYear(), tempDate.getMonth() - 1)
         .toLocaleString('pt-BR', { month: 'short', year: '2-digit' })
         .toLocaleLowerCase();
     return date.substring(0, 3) + '/' + date.substring(date.length - 2);
-}
+};
 
 const getLastMonthYearLong = () => {
     const tempDate = new Date();
-    const date = (new Date(tempDate.getFullYear(), tempDate.getMonth() - 1))
+    const date = new Date(tempDate.getFullYear(), tempDate.getMonth() - 1)
         .toLocaleString('pt-BR', { month: 'long', year: 'numeric' })
         .toLocaleLowerCase();
     const formatedDate = date.split(' ');
     return `${formatedDate[0]}, ${formatedDate[2]}`;
-}
+};
 const getCurrentDate = () => {
-    return new Date().toISOString().split('T')[0]
-}
+    return new Date().toISOString().split('T')[0];
+};
+
+const getCurrentDateForScheduleHistory = () => {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = ('0' + (date.getMonth() + 1)).slice(-2);
+    let day = ('0' + date.getDate()).slice(-2);
+    let timestamp = `${year}-${month}-${day} 00:00:00`;
+    let timezone = `-03:00`;
+    return `${timestamp}${timezone}`;
+};
 
 module.exports = {
     getDate,
@@ -59,5 +72,6 @@ module.exports = {
     getCurrentYear,
     getLastMonthYearShort,
     getLastMonthYearLong,
-    getCurrentDate
-}
+    getCurrentDate,
+    getCurrentDateForScheduleHistory,
+};
